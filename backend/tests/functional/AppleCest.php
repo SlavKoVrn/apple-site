77608064bfc8e4046b2dff0506d389c8d9999908
tusba <11007086+tusba@users.noreply.github.com>
2021-02-01 16:04:21 +0600

    Add functional tests for eating an apple

--- backend/tests/functional/AppleCest.php
+++ backend/tests/functional/AppleCest.php
@@ -56,11 +56,47 @@ class AppleCest
         $I->seeElement('tr[data-key="1"] .glyphicon-hand-down');
 
         $I->expectTo('see no button to drop a fallen apple in grid view');
-        $I->dontSeeElement('tr[data-key="3"] .glyphicon-hand-down');
+        $I->dontSeeElement('tr[data-key="6"] .glyphicon-hand-down');
 
         $I->amGoingTo('drop a hanging apple');
         $I->click('[title="Fall"]', 'tr[data-key="2"]');
         $I->expectTo('see no button to drop an apple after falling in grid view');
         $I->dontSeeElement('tr[data-key="2"] .glyphicon-hand-down');
+    }
+
+    public function testCanEat(FunctionalTester $I)
+    {
+        $I->expectTo('see no form to eat a hanging apple in grid view');
+        $I->dontSee('Eat', 'tr[data-key="2"] form .btn');
+
+        $I->expectTo('see no form to eat a rotten apple in grid view');
+        $I->dontSee('Eat', 'tr[data-key="8"] form .btn');
+
+        $I->expectTo('see a form to eat a fallen apple in grid view');
+        $I->see('Eat', 'tr[data-key="6"] form .btn');
+    }
+
+    public function testEat(FunctionalTester $I)
+    {
+        $I->amGoingTo('eat a fallen apple');
+        $I->fillField('tr[data-key="6"] [name="AppleEatingForm[eatenPercent]"]', 32);
+        $I->click('Eat', 'tr[data-key="6"]');
+        $I->expectTo('see an alert with successful result');
+        $I->see('31% left', '.alert.alert-success');
+
+        $I->expectTo('see a form to eat a fallen apple in grid view');
+        $I->see('Eat', 'tr[data-key="6"] form .btn');
+    }
+
+    public function testEatUp(FunctionalTester $I)
+    {
+        $I->amGoingTo('eat up a fallen apple');
+        $I->fillField('tr[data-key="6"] [name="AppleEatingForm[eatenPercent]"]', 64);
+        $I->click('Eat', 'tr[data-key="6"]');
+        $I->expectTo('see an alert with successful result');
+        $I->see('The apple was eaten whole', '.alert.alert-success');
+
+        $I->expectTo('see no form to eat a fallen apple in grid view');
+        $I->dontSee('Eat', 'tr[data-key="6"] form .btn');
     }
 }
